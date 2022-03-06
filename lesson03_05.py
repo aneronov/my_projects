153f634491ae12eab8059bce854e7cb296ed8a2c
aneronov <98848061+aneronov@users.noreply.github.com>
2022-03-06 12:37:08 +0300

    Add files via upload

---
+++ lesson03_05.py
@@ -0,0 +1,18 @@
+x = []
+q = False
+print('для завершения программы введите "Q"')
+while True:
+    a = input('введите числа через пробел: ')
+    b = a.split()
+    for i in b:
+        if i.isdigit():
+            x.append(int(i))
+        elif i.upper() == "Q":
+            q = True
+            break
+    print('сумма всех введенных цифр = ', sum(x))
+    if q:
+        print('работа программы завершена')
+        break
+
+
